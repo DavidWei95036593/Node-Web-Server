11c857f6450801632c70514c2eecb901e404b049
DavidWei95036593 <dw36593@pausd.us>
2018-02-09 13:49:59 -0800

    making header

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require(`express`);
+const hbs = require(`hbs`);
+const path = require(`path`);
 
 const app= express();
+app.set(`view engine`,`hbs`);
+hbs.registerPartials(path.join(__dirname,"views","partials"))
 
-app.get(`/`,(req,res)=>{
-  res.send({
-    course:`Web Development`,
-    school:`Paly`
+  app.get(`/`,(req,res)=>{
+    res.render(`index.hbs`,{
+      about:`About Me`,
+      content:`Jeremy is a despacito`,
+      school:`Paly`,
+      year:new Date().getFullYear(),
+      pageTitle:`Home`
+    });
+  //sentfile sent it to the excate location
+  // app.sendFile(__dirname+"/public");
   })
-})
-app.listen(3000,()=>{
+  //this tells express to make the content of the  directory make it visible
+  // app.use(express.static(__dirname + "/public"));
+
+  app.get(`/about`,(req,res)=>{
+    res.render(`about.hbs`,{
+      about:`About Me`,
+      content:`Jeremy is a despacito`,
+      school:`Gunn`,
+      year:new Date().getFullYear(),
+      pageTitle:`About`
+    });
+  })
+  // app.use(express.static(__dirname + "/public"));
+
+
+  app.listen(3000,()=>{
   console.log("Listening on port 3000!");
 })
