e7b6e4c9ab5932cee2d373e6a2aacb61397b66c9
DavidWei95036593 <dw36593@pausd.us>
2018-02-12 13:28:30 -0800

    finish the web server

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const path = require(`path`);
 const app= express();
 app.set(`view engine`,`hbs`);
 hbs.registerPartials(path.join(__dirname,"views","partials"))
-
+hbs.registerHelper(`getCurrentYear`,()=>{
+  // return "HIII";
+  return new Date().getFullYear();
+})
+hbs.registerHelper(`yell`,(word)=>{
+    return word.substr(0,1).toUpperCase()+word.substr(1).toLowerCase()+"!!!";
+})
   app.get(`/`,(req,res)=>{
     res.render(`index.hbs`,{
       about:`About Me`,
       content:`Jeremy is a despacito`,
       school:`Paly`,
-      year:new Date().getFullYear(),
       pageTitle:`Home`
     });
   //sentfile sent it to the excate location
@@ -25,12 +30,20 @@ hbs.registerPartials(path.join(__dirname,"views","partials"))
       about:`About Me`,
       content:`Jeremy is a despacito`,
       school:`Gunn`,
-      year:new Date().getFullYear(),
       pageTitle:`About`
     });
   })
   // app.use(express.static(__dirname + "/public"));
-
+  //this means to get every characters that has ing in it
+  app.get(/.*ing/,(req,res)=>{
+    res.send({
+      year:new Date().getFullYear(),
+      originalUrl:req.originalUrl,
+      queryParams: req.query,
+      school:req.query.school,
+      clsss:req.query.class
+    })
+  })
 
   app.listen(3000,()=>{
   console.log("Listening on port 3000!");
